314ff9e8141698544d487283df90f9712317eee2
Samuel Gbafa <skgbafa@gmail.com>
2025-03-03 12:57:59 -0700

    remove unused files

--- lib/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod authorization;
-pub mod resolver;
-pub mod resource;
-
-pub use cacaos;
-pub use libipld;
-pub use siwe_recap;
-pub use ssi;
--- sdk/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod authorization;
-pub mod serde_siwe;
-pub mod session;
-pub mod siwe_utils;
-pub mod util;
