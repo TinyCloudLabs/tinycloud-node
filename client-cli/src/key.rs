305e5ec2a0ca8595902ef1c897ac889f05cd6774
chunningham <c.a.cunningham6@gmail.com>
2025-06-27 17:59:03 +0200

    remove unused crypto stuff, use ssi more

--- client-cli/src/key.rs
+++ client-cli/src/key.rs
@@ -1,11 +1,10 @@
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
-use sha3::{Digest, Keccak256};
 use tinycloud_lib::ssi::{
-    crypto::k256::{elliptic_curve::sec1::ToEncodedPoint, PublicKey, SecretKey},
-    dids::{DIDBuf, DID},
-    jwk::{Base64urlUInt, ECParams, Params, JWK},
+    crypto::k256::SecretKey,
+    dids::{DIDBuf, DIDResolver, DID, DIDPKH},
+    jwk::{ECParams, Params, JWK},
 };
 
 use crate::error::CliError;
@@ -15,7 +14,6 @@ pub struct EthereumKey {
     secret_key: SecretKey,
     jwk: JWK,
     did: DIDBuf,
-    address: String,
 }
 
 impl EthereumKey {
@@ -24,21 +22,18 @@ impl EthereumKey {
         let secret_key = SecretKey::from_bytes(&private_key.into())
             .map_err(|e| anyhow!("Invalid secp256k1 private key: {}", e))?;
 
-        // Generate Ethereum address
-        let public_key = secret_key.public_key();
-        let address = ethereum_address_from_public_key(&public_key)?;
-
         // Create JWK
-        let jwk = create_secp256k1_jwk(&secret_key)?;
+        let mut jwk: JWK = Params::EC(ECParams::from(&secret_key)).into();
+        jwk.algorithm = Some(tinycloud_lib::ssi::jwk::Algorithm::ES256KR);
 
         // Generate DID
-        let did = format!("did:pkh:eip155:1:{}", address).parse()?;
+        // let did = format!("did:pkh:eip155:1:{}", address).parse()?;
+        let did = DIDPKH::generate(&jwk, "eip155:1")?;
 
         Ok(Self {
             secret_key,
             jwk,
             did,
-            address,
         })
     }
 
@@ -50,10 +45,6 @@ impl EthereumKey {
         &self.did
     }
 
-    pub fn get_address(&self) -> &str {
-        &self.address
-    }
-
     pub fn get_verification_method(&self) -> String {
         format!("{}#blockchainAccountId", self.did)
     }
@@ -61,47 +52,6 @@ impl EthereumKey {
     pub fn get_secret_key(&self) -> &SecretKey {
         &self.secret_key
     }
-}
-
-fn ethereum_address_from_public_key(public_key: &PublicKey) -> Result<String> {
-    // Get uncompressed public key (65 bytes: 0x04 + 32 bytes x + 32 bytes y)
-    let public_key_point = public_key.to_encoded_point(false);
-    let public_key_bytes = public_key_point.as_bytes();
-
-    // Skip the 0x04 prefix and hash the remaining 64 bytes
-    let public_key_hash = Keccak256::digest(&public_key_bytes[1..]);
-
-    // Take last 20 bytes and format as hex
-    let address_bytes = &public_key_hash[12..];
-    let address_hex = hex::encode(address_bytes);
-
-    // Apply EIP-55 checksum encoding
-    let checksum_hash = Keccak256::digest(address_hex.as_bytes());
-    let mut checksummed_address = String::with_capacity(42);
-    checksummed_address.push_str("0x");
-
-    for (i, c) in address_hex.chars().enumerate() {
-        if c.is_ascii_digit() {
-            checksummed_address.push(c);
-        } else {
-            // Check if the corresponding nibble in the hash is >= 8
-            let hash_byte = checksum_hash[i / 2];
-            let nibble = if i % 2 == 0 {
-                hash_byte >> 4
-            } else {
-                hash_byte & 0xf
-            };
-
-            let checksum_char = if nibble >= 8 {
-                c.to_ascii_uppercase()
-            } else {
-                c.to_ascii_lowercase()
-            };
-            checksummed_address.push(checksum_char);
-        }
-    }
-
-    Ok(checksummed_address)
 }
 
 impl FromStr for EthereumKey {
@@ -140,10 +90,6 @@ impl FromStr for EthereumKey {
 
         Ok(EthereumKey::new(private_key)?)
     }
-}
-
-fn create_secp256k1_jwk(secret_key: &SecretKey) -> Result<JWK> {
-    Ok(Params::EC(ECParams::from(secret_key)).into())
 }
 
 #[cfg(test)]
